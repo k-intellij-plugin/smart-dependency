6c95240071d39ce6262646b5445e367198bf5f4f
GitHub Action <action@github.com>
2025-09-17 11:27:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/sungjun1116/smartdependency/MyBundle.kt
+++ src/main/kotlin/com/github/sungjun1116/smartdependency/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sungjun1116.smartdependency
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sungjun1116/smartdependency/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/sungjun1116/smartdependency/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.sungjun1116.smartdependency.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
